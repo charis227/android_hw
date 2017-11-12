413fa9ae89609bfef8fba72d0f7ab57ac3d7d3c8
jiun\jiunoh <juneoh227@gmail.com>
2017-11-12 23:49:47 +0900

    새 센서 준비

--- s1512602_06_OhJiun/app/src/main/java/com/example/android/s1512602_06_ohjiun/MainActivity.java
+++ s1512602_06_OhJiun/app/src/main/java/com/example/android/s1512602_06_ohjiun/MainActivity.java
@@ -5,22 +5,23 @@ import android.graphics.Point;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
-import android.hardware.SensorListener;
 import android.hardware.SensorManager;
-import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    TextView azimuth;
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+    TextView tvAzimuth;
     SensorManager sensorManager;
-    Sensor sensor_orientation;
-    Sensor magnetic, accel;
+    Sensor sensorMagnetic, sensorAccel;
     ImageView compass;
+    float azimuth, pitch, roll;
     float prev;
+    float[] gravityArr;
+    float[] magneticArr;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,23 +42,24 @@ public class MainActivity extends AppCompatActivity {
 
         int x = screenWidth/2 - compassWidth/2;
         int y = screenHeight/2 - compassHeight/2;
-        compass.setX(x);
-        compass.setY(y);
+//        compass.setX(x);
+//        compass.setY(y);
 
-        azimuth = (TextView) findViewById(R.id.azimuth);
+        tvAzimuth = (TextView) findViewById(R.id.azimuth);
         prev = 0.0f;
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        magnetic = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        sensorAccel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorMagnetic = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
 //        sensor_orientation = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
     }
 
-    protected void onResum() {
+    protected void onResume() {
         super.onResume();
-        sensorManager.registerListener(this, accel,SensorManager.SENSOR_DELAY_NORMAL);
-/*        sensorManager.registerListener((SensorEventListener) this, sensor_orientation,
+        sensorManager.registerListener(this, sensorAccel,SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, sensorMagnetic, SensorManager.SENSOR_DELAY_NORMAL);
+        /*        sensorManager.registerListener((SensorEventListener) this, sensor_orientation,
                 SensorManager.SENSOR_DELAY_NORMAL); */
     }
 
@@ -70,9 +72,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+            gravityArr = event.values;
+        else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+            magneticArr = event.values;
+
+        float R[] = new float[9];
+        float I[] = new float[9];
+        SensorManager.getRotationMatrix(R, I, gravityArr, magneticArr);
+        float orientation[] = new float[3];
+        SensorManager.getOrientation(R, orientation);
+        azimuth = (float) Math.toDegrees(orientation[0]);
+        pitch = (float) Math.toDegrees(orientation[1]);
+        roll = (float) Math.toDegrees(orientation[2]);
+
+        String result;
+        result = "Azimuth:" + azimuth;
+        tvAzimuth.setText(result);
+
         float newAngle = event.values[0];
         String text = Float.toString(newAngle);
-       azimuth.setText(text);
+       tvAzimuth.setText(text);
         ObjectAnimator rotator = ObjectAnimator.ofFloat(compass,
                 "rotation", prev, newAngle);
         rotator.start();
