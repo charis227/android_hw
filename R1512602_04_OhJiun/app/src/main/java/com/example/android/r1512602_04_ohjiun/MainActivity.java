3cce2cd4147161c8d771134c9c7e8776c8ff2c95
jiun\jiunoh <juneoh227@gmail.com>
2017-11-25 14:33:09 +0900

    생성

---
+++ R1512602_04_OhJiun/app/src/main/java/com/example/android/r1512602_04_ohjiun/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.r1512602_04_ohjiun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
